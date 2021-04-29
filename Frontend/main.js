0ad798a7a9476ce38b452c2d816e4fb8464a29a7
Rex Elardo <rexelardo@gmail.com>
2021-04-29 18:15:48 -0400

    OK really not sure why the hideElement is not working

--- Frontend/main.js
+++ Frontend/main.js
@@ -2,14 +2,14 @@ Moralis.initialize("3IcvId63X3g2jWHA2t2OCOFDHmfOw3PbF9etB6Mt");
 Moralis.serverURL = 'https://k8lq9f7lkimp.moralis.io:2053/server'
 
 init = async () => {
-    hideElement(userInfo)
+    hideElement(userInfo);
     window.web3 = await Morialis.web3.enable(); 
-    InitUser();
+    initUser();
 }
 
 
 
-InitUser = async () => {
+initUser = async () => {
     if (await Moralis.User.current()){
         hideElement(userConnectButton);
         showElement(userProfileButton);
@@ -22,11 +22,30 @@ InitUser = async () => {
 login = async () => {
     try {
         await Moralis.Web3.authenticate();
-        InitUser();
+        initUser();
     } catch (error) {
         alert(error)
     }
 }
+
+
+logout = async () => {
+    await Moralis.user.logOut();
+    hideElement(userInfo);
+    initUser();
+}
+
+
+OpenUserInfo = async() =>{
+    user = await Moralis.user.current();
+    if (user){
+        showElement(userInfo)
+    }else{
+        login();
+    }
+}
+
+
 
 
 hideElement = (element) => element.style.display = 'none';
@@ -36,8 +55,10 @@ const userConnectButton = document.getElementById('btnConnect');
 userConnectButton.onclick = login;
 
 const userProfileButton = document.getElementById('btnUserInfo');
+userProfileButton.onclick = OpenUserInfo;
 
 const userInfo = document.getElementById('userInfo');
-
+document.getElementById('btnCloseUserInfo').onclick = () => hideElement(userInfo);
+document.getElementById('btnLogout').onclick = logout;
 init();
 
